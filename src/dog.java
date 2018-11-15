adabb67dbdff404fb792ffd3a799fe78dd2c7c59
BT_1N3_15@BKTECH.LOCAL <BT_1N3_15@BKTECH.LOCAL>
2018-11-15 14:47:13 -0500

    Initial commit

---
+++ src/dog.java
@@ -0,0 +1,15 @@
+public class dog implements Horse {
+    private int weight;
+    private String name;
+
+    public dog (int weight, String name)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+    public int getWeight()
+    {
+        
+    }
+
+}
